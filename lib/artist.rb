3a5d56df1866c85a69ecb0561f935229a170ac3b
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-04 09:20:06 -0700

    Update attributes in artist class

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,14 @@
 class Artist
+  attr_reader :id, :name, :born, :died, :country
   def initialize(info)
-    @info = info
+    @id = info[:id]
+    @name = info[:name]
+    @born = info[:born]
+    @died = info[:died]
+    @country = info[:country]
+  end
+
+  def age_at_death
+    @died.to_i - @born.to_i
   end
 end
