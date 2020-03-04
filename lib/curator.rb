18674e7dd9e214e1b5aead248632a33acbc3c8a6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-04 09:50:23 -0700

    Create helper method of photo_by_artist_id and photos by artist hash

--- lib/curator.rb
+++ lib/curator.rb
@@ -19,4 +19,18 @@ class Curator
     end
   end
 
+  def photo_by_artist_id(id)
+    @photographs.find_all do |photo|
+      photo.artist_id == id
+    end
+  end
+
+  def photographs_by_artist
+    artist_photos = {}
+    artists.each do |artist|
+      artist_photos[artist] = photo_by_artist_id(artist.id)
+    end
+    artist_photos
+  end
+
 end
