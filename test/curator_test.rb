df030e47b339d204f0e957f83aec4a00f1ed34d0
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-04 09:26:54 -0700

    Create add photo method and test

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -9,4 +9,29 @@ class CuratorTest < Minitest::Test
     curator = Curator.new
     assert_instance_of Curator, curator
   end
+
+  def test_it_has_no_photographs_when_new
+    curator = Curator.new
+    assert_equal [], curator.photographs
+  end
+
+  def test_it_can_add_photographs
+    curator = Curator.new
+    photo_1 = Photograph.new({
+         id: "1",
+         name: "Rue Mouffetard, Paris (Boy with Bottles)",
+         artist_id: "1",
+         year: "1954"
+     })
+     photo_2 = Photograph.new({
+          id: "2",
+          name: "Moonrise, Hernandez",
+          artist_id: "2",
+          year: "1941"
+     })
+
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    assert_equal [photo_1, photo_2], curator.photographs
+  end
 end
